7bd1629e3b46430a8c323104ea5613fbafb9aa85
shekarbabug <shekarbabug@gmail.com>
2023-07-24 19:10:54 +0400

    added main module

---
+++ src/main/java/com/main/build/modules/DemoApplication.java
@@ -0,0 +1,8 @@
+package com.main.build.modules;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/test/java/com/main/build/modules/DemoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.main.build.modules;
+
+class DemoApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
