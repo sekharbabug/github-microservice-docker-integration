e218812bb6ab9833a912c589624620120216efe4
sekharbabug <sekhar.babu260@gmail.com>
2023-07-24 20:02:06 +0400

    Update PaymentServiceApplicationTests.java
    payment service added

--- payment-service/src/test/java/com/javatechie/spring/eureaka/provider/api/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/javatechie/spring/eureaka/provider/api/PaymentServiceApplicationTests.java
@@ -1,15 +1,10 @@
 package com.javatechie.spring.eureaka.provider.api;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class PaymentServiceApplicationTests {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
